95c5b3708d2da4589c59d5d67d45f96b4a15c316
SarahHayat <saraahyt@gmail.com>
2019-11-25 11:18:12 +0100

    commands help and exit

--- src/com/company/Commands.java
+++ src/com/company/Commands.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class Commands {
+
+    public static void help() {
+        String help = "Débrouille toi !";
+        System.out.println(help);
+
+    }
+
+    public static void exit(){
+
+        String exit = "Je sors!";
+        System.out.println(exit);
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Application has started"); //test message
+
+        Scanner sc = new Scanner(System.in);
+        int str = sc.nextInt();
+        if( str == 1){
+            Commands.help();
+        }else if ( str == 0){
+            Commands.exit();
+        }
+
     }
 }
