42c74c79a54028702314cac152a48d3743982066
Laurent <laurent.sem@edu.itescia.fr>
2019-11-26 16:01:27 +0100

    add Javadoc on Wizard.java

--- src/com/company/Wizard.java
+++ src/com/company/Wizard.java
@@ -12,19 +12,44 @@ public Wizard (String name, int damage, int maxLifePoints, int initiative, int m
     this.currentMagicDamage = maxMagicDamage*2;
     // Double the current magic damage for the first round
 }
-
+    /**
+     * Get the current Magic Damage
+     * @param
+     * @return currentMagicDamage
+     *
+     */
     public int getCurrentMagicDamage() { return this.currentMagicDamage/2; }
+
+    /**
+     * Get the current Magic Damage
+     * @param
+     * @return currentMagicDamage
+     *
+     */
 
     public int getMaxMagicDamage() { return this.maxMagicDamage/2; }
 
+    /**
+     * Get the Wizard damage
+     * Divide the magic damage by 2 and return it with the damage
+     * @param damage
+     * @return currentMagicDamage
+     *
+     */
     public int getDamage(int damage) {
         this.currentMagicDamage = currentMagicDamage/2;
         return this.currentMagicDamage + damage;
-    } // Divide the magic damage by 2 and return it with the damage
+    }
 
+    /**
+     * Reset the Wizard damage
+     * Double the current magic damage for the first round
+     * @param
+     *
+     */
     public void resetMagicDamage(){
         this.currentMagicDamage = maxMagicDamage*2;
-        // Double the current magic damage for the first round
+
     }
 
     public String toString(){
