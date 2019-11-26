cfce4385b5ccfed3b8c7cb58c4cb2ab81f51eefe
Laurent <laurent.sem@edu.itescia.fr>
2019-11-26 14:32:53 +0100

    Added special luck methods for the thief
    Dodge and critical damage outputs now possible

--- src/com/company/Thief.java
+++ src/com/company/Thief.java
@@ -1,28 +1,57 @@
 package com.company;
 
+import java.util.Random;
+
 public class Thief extends Characters {
 
-    private int dodge;
-    private int criticalDamage;
+    private int probDodge;
+    private int probCritical;
+    private boolean criticalPreviousTurn;
 
-    public Thief (String name, int damage, int maxLifePoints, int initiative, int dodge){
+    public Thief (String name, int damage, int maxLifePoints, int initiative, int probDodge, int probCritical){
 
         super(name, maxLifePoints, damage, initiative);
-        this.dodge = dodge;
+        this.probDodge = probDodge;
+        this.probCritical = probCritical;
+        this.criticalPreviousTurn = false;
     }
 
-    public int getDodge() {
+    public int getProbDodge() { return this.probDodge; }
 
-        return this.dodge;
+    public int getProbCritical() { return this.probCritical; }
+
+    public void inflictDamage(int damage){
+
+        Random random = new Random();
+        int luck = random.nextInt(101);
+        // random number between 0 and 100
+
+        if (luck <= this.getProbDodge()){
+            // Do nothing, the thief dodges the attack
+        }
+        else {
+            this.inflictDamage(damage);
+        }
     }
 
-    public int getCriticalDamage() {
+    public int getDamage(){
 
-        return this.criticalDamage;
+        Random random = new Random();
+        int luck = random.nextInt(101);
+        // random number between 0 and 100
+
+        if (luck <= this.getProbCritical() && criticalPreviousTurn == false){
+            criticalPreviousTurn = true;
+            return super.getDamage()*2;
+        }
+        else {
+            criticalPreviousTurn = false;
+            return super.getDamage();
+        }
     }
 
     public String toString(){
-        return (super.toString() + "Critical damage : " + this.getCriticalDamage() + "\n"); // Send character's information
-
+        return (super.toString() + "Dodge probability : " + this.getProbDodge() + "\n"
+                + "Critical hit probability : " + this.getProbCritical() + "\n");// Send character's information
     }
 }
