277bccf68fdda93fa99077dfc24514bf0c94312e
Laurent <laurent.sem@edu.itescia.fr>
2019-11-26 13:34:05 +0100

    finish Wizard.java

--- src/com/company/Wizard.java
+++ src/com/company/Wizard.java
@@ -2,15 +2,37 @@ package com.company;
 
 public class Wizard extends Characters {
 
-    private int magicDamage;
+    private int maxMagicDamage;
+    private int currentMagicDamage;
 
-public Wizard (String name, int damage, int maxLifePoints, int initiative, int magicDamage){
+
+public Wizard (String name, int damage, int maxLifePoints, int initiative, int maxMagicDamage){
 
     super(name, maxLifePoints, damage, initiative);
-    this.magicDamage = magicDamage;
+    this.currentMagicDamage = maxMagicDamage*2;
+    // Double the current magic damage for the first round
 }
 
-    public int getMagicDamage(int Damage) {
-        return this.magicDamage + Damage;
+    public int getCurrentMagicDamage(int damage) {
+         return (this.currentMagicDamage);
     }
+
+    public int getMaxMagicDamage(int damage) {
+        return (this.maxMagicDamage);
+    }
+
+    public int getDamage(int damage) {
+        return (this.currentMagicDamage)/2 + damage;
+    } // Divide the magic damage by 2 each round
+
+    public void resetMagicDamage(){
+        this.currentMagicDamage = maxMagicDamage*2;
+        // Double the current magic damage for the first round
+    }
+
+    public String toString(){
+        return (super.toString() + "Base Magic Damage : " + this.maxMagicDamage+ "\n"); // Send character's information
+
+    }
+
 }
